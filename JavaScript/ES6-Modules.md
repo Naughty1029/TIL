83d55b060f0b26ffbaf6e6640b3bafa295062a84
watanabe_masahiro02 <watanabe_masahiro@5star.co.jp>
2022-01-14 15:44:52 +0900

    途中

--- JavaScript/ES6-Modules.md
+++
@@ -1,9 +0,0 @@
-## What Today I Learned...
-JavaScript Module.
-
-## Why do we need module?
-
-
-
-## Reference Link
-- [JavaScript モジュール](https://developer.mozilla.org/ja/docs/Web/JavaScript/Guide/Modules)
---
+++ JavaScript/React/beginner-basic/src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(
+  <React.StrictMode>
+    {/* prop はコンポーネント内に記述され、HTML属性と同じ構文を使用する — prop="value" */}
+    <App subject="Clarice" />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
