4317ac4c73b374e19bce7e6304cee167d65d04d0
Naughty1029 <masahiro.watanabe02@gmail.com>
2022-02-05 18:43:59 +0900

    python

---
+++ Python/python_test/test.py
@@ -0,0 +1,22 @@
+#コメント
+print("Hello World")
+
+#演算
+print(1+1)
+print(1-1)
+print(1*2)
+print(10/2)
+print(5%3)
+
+# 変数→文字化と共通化がメリット
+string = "Hello World"
+integer = 1
+float = 1.1
+print(string)
+
+# 変数の型
+print(type(string))
+print(type(integer))
+print(type(float))
+
+# 24:00まで
