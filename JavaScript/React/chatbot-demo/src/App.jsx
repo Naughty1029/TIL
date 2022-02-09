58cd3409b64a8c32c4489040455bdf3b04da78c8
Naughty1029 <masahiro.watanabe02@gmail.com>
2022-02-09 17:36:09 +0900

    chatbot途中

---
+++ JavaScript/React/chatbot-demo/src/App.jsx
@@ -0,0 +1,39 @@
+import defaultDataset from './dataset';
+import "./assets/styles/style.css";
+import React from 'react';
+import { AnswersList } from './components';
+
+export default class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      answers:[],
+      chats:[],
+      currentId:"init",
+      dataset:defaultDataset,
+      open:false
+    }
+  }
+
+  initAnswer = ()=>{
+    const initDataset = this.state.dataset[this.state.currentId];
+    const initAnswers = initDataset.answers;
+    this.setState({
+      answers:initAnswers,
+    })
+  }
+
+  componentDidMount(){
+    this.initAnswer()
+  }
+
+  render(){
+    return (
+      <section className='c-section'>
+        <div className='c-box'>
+          <AnswersList answers={this.state.answers}/>
+        </div>
+      </section>
+    );
+  }
+}
