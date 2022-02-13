dae6bc597357f091d45a27a562976de7d4d6600a
Naughty1029 <masahiro.watanabe02@gmail.com>
2022-02-13 12:40:09 +0900

    validation

--- PHP/TodoApp/app/Http/Controllers/TaskController.php
+++ PHP/TodoApp/app/Http/Controllers/TaskController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\StoreTaskRequest;
+use App\Http\Requests\TaskRequest;
 use App\Http\Requests\UpdateTaskRequest;
 use App\Models\Task;
 
@@ -13,15 +14,10 @@ class TaskController extends Controller
         return Task::orderByDesc("id")->get();
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \App\Http\Requests\StoreTaskRequest  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(StoreTaskRequest $request)
+    public function store(TaskRequest $request)
     {
-        //
+        $task = Task::create($request->all());
+        return $task ? response()->json($task,201) : response()->json([],500);
     }
 
     /**
@@ -35,26 +31,14 @@ class TaskController extends Controller
         //
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \App\Http\Requests\UpdateTaskRequest  $request
-     * @param  \App\Models\Task  $task
-     * @return \Illuminate\Http\Response
-     */
-    public function update(UpdateTaskRequest $request, Task $task)
+    public function update(TaskRequest $request, Task $task)
     {
-        //
+        $task->title = $request->title;
+        return $task->update() ? response()->json($task) : response()->json([],500);
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Task  $task
-     * @return \Illuminate\Http\Response
-     */
     public function destroy(Task $task)
     {
-        //
+        return $task->delete() ? response()->json($task) : response()->json([],500);
     }
 }
