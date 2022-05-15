d3db2cec3f4fe550bbd9097fe7aabe804e205338
Masahiro Watanabe <56257897+Naughty1029@users.noreply.github.com>
2022-05-15 18:17:53 +0900

    Delete JavaScript/TypeScript/ts-basic directory

--- JavaScript/TypeScript/ts-basic/src/basic/any.ts
+++
@@ -1,7 +0,0 @@
-export default function anySample() {
-  let name: any = 'name'
-  console.log('any sample 1:', typeof name, name)
-
-  name = 28
-  console.log('any sample 2:', typeof name, name)
-}
